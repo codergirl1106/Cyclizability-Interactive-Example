62edc13337a988ae4f9e77c2d1b1056fac9c1aa0
codergirl1106 <68753302+codergirl1106@users.noreply.github.com>
2024-10-27 10:32:09 -0400

    interactive.py

--- interactive.py
+++ interactive.py
@@ -175,7 +175,7 @@ def trig(x, *args): # x = [C0, amp, psi]
 def show_st_3dmol(pdb_code,original_pdb,cartoon_style="oval",
                   cartoon_radius=0.2,cartoon_color="lightgray",zoom=1,spin_on=False):
     
-    view = py3Dmol.view(width=450, height=450)
+    view = py3Dmol.view(width=425, height=425)
         
     view.addModelsAsFrames(pdb_code)
     view.addModelsAsFrames(original_pdb)
@@ -201,7 +201,7 @@ def show_st_3dmol(pdb_code,original_pdb,cartoon_style="oval",
     view.spin(spin_on)
     view.zoom(zoom)
 
-    showmol(view, height=450, width=450)
+    showmol(view, height=425, width=425)
     return 0
 
 def helpercode(model_num: int, seqlist: dict, pool, sequence):
