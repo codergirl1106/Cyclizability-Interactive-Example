90018b4a8c55f8677e2c08bd3760ab381b443791
codergirl1106 <68753302+codergirl1106@users.noreply.github.com>
2024-10-27 10:01:58 -0400

    interactive.py

--- interactive.py
+++ interactive.py
@@ -36,7 +36,7 @@ def my_cache(f):
 
 @st.cache_data
 def load_model(modelnum: int):
-    return keras.layers.TFSMLayer(f"./adapter-free-Model/C{modelnum}free", call_endpoint='serving_default')
+    return keras.layers.TFSMLayer(f"./adapter-free-Model/C{modelnum}free")
 
 def pred(model, pool):
     input = np.zeros((len(pool), 200), dtype = np.single)
